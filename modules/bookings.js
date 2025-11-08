2d5731c517cc14c47c4b5faea9f2c4017186c8c5
mazen_devo <mazen.ayman.mohamd@gmail.com>
2025-11-08 07:40:53 -0500

    - Add bookings module

---
+++ modules/bookings.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import Counter from "./counter.js";
+import { UserInfo } from "firebase-admin/auth";
+
+const bookingsSchema = new mongoose.Schema({
+  _id: { type: String },
+  userId: { type: String },
+  courtId: { type: String },
+  bookingDate: { type: Date },
+  times: { type: [] },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isNew) return next();
+
+  try {
+    const counter = await Counter.findOneAndUpdate(
+      { name: "booking_id" },
+      { $inc: { value: 1 } },
+      { new: true, upsert: true }
+    );
+
+    const newId = `BK-${counter.value.toString().padStart(6, "0")}`;
+    this._id = newId;
+    next();
+  } catch (err) {
+    console.error("Error in generating ID:", err);
+    next(err);
+  }
+});
+
+const Bookings = mongoose.model("Bookings", bookingsSchema);
+export default User;
