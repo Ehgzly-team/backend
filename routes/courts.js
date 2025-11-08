369ca74977f43d851754a41ce29bda2c985463a7
mazen_devo <mazen.ayman.mohamd@gmail.com>
2025-11-07 23:45:56 -0500

    - Found reason of error - this line: import serviceAccount from "../config/ehgzly.json"assert {type:'json'}; - why it's represent error ? - cuz vercel using old version from node :( - So i replaced this line with another one work in any node version GG

--- routes/courts.js
+++ routes/courts.js
@@ -4,7 +4,10 @@ import admin from "firebase-admin";
 import { getStorage } from "firebase-admin/storage";
 import path from "path";
 import courtModules from "../modules/court.js";
-import serviceAccount from "../config/ehgzly.json"assert {type:'json'};
+import { readFileSync } from "fs";
+const serviceAccount = JSON.parse(
+  readFileSync(new URL('../config/ehgzly.json', import.meta.url), 'utf8')
+);
 const router = express.Router();
 
 
